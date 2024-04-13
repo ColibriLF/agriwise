97761ae07b479f4a5486e052f22aed907993e187
ColibriLF <24692@ipportalegre.pt>
2024-04-13 14:44:37 +0100

    Admin e alteração à BD

--- agriwise/app/admin.py
+++ agriwise/app/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import User, Parcela, Registo
+
+#admin.site.register(User)
+#admin.site.register(Parcela)
+#admin.site.register(Registo)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'first_name', 'last_name', 'email')
+
+class RegistoInLine(admin.TabularInline):
+    model = Registo
+    extra = 1
+
+@admin.register(Parcela)
+class ParcelaAdmin(admin.ModelAdmin):
+    parcela_display = ('user', 'nome', 'area')
+    inlines = [RegistoInLine]
+    search_fields = ['parcela__nome']
+
+@admin.register(Registo)
+class RegistoAdmin(admin.ModelAdmin):
+    registo_display = ('data', 'parcela', 'produto', 'dose')
+    search_fields = ['Registo__data', 'Registo__produto']
 
 # Register your models here.
--- agriwise/app/models.py
+++ agriwise/app/models.py
@@ -7,4 +7,20 @@ class User(AbstractUser):
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
 
-#descrever as tabelas
+class Parcela(models.Model):
+    nome = models.CharField(max_length=100)
+    area = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='parcelas')
+
+    def __str__(self):
+        return f'{self.nome}'
+
+
+class Registo(models.Model):
+    data = models.DateField(auto_now_add=True)
+    parcela = models.ForeignKey(Parcela, on_delete=models.CASCADE, related_name='registos')
+    produto = models.CharField(max_length=100)
+    dose = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.data} {self.parcela} {self.produto} {self.dose}'
