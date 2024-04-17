2f4e3f264108d72b120ccf01d7e9593b8930e3b0
JoaoAAlvesPT <166857293+JoaoAAlvesPT@users.noreply.github.com>
2024-04-17 22:08:43 +0100

    falta os restantes parametros

--- agriwise/api/views.py
+++ agriwise/api/views.py
@@ -31,3 +31,17 @@ class DetailPARCELAS(APIView):
         parcela = get_object_or_404(Parcela, pk=parcela_id, user=request.user)
         serializer = ParcelaDetailSerializer(parcela, many=False)
         return Response(serializer.data)
+
+    def delete(self,request, parcela_id):
+        parcela = get_object_or_404(Parcela, pk=parcela_id, user=request.user)
+        parcela.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+    def put(self,request, parcela_id):
+        parcela = get_object_or_404(Parcela, pk=parcela_id, user=request.user)
+        serializer = ParcelaDetailSerializer(parcela, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
