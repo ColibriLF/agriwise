746ec957a9e673a36d2b2dcdf8adbd353586a91c
JoaoAAlvesPT <166857293+JoaoAAlvesPT@users.noreply.github.com>
2024-04-20 16:13:27 +0100

    GET,POST,PUT,DELETE nos Registos

--- agriwise/api/urls.py
+++ agriwise/api/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('parcelas/', views.ListPARCELAS.as_view()),
-    path('parcelas/<int:parcelas_id>/', views.DetailPARCELAS.as_view())
+    path('parcelas/<int:parcelas_id>/', views.DetailPARCELAS.as_view()),
+# COLOCAR OS REGISTOS #
+    path('parcelas/<int:parcelas_id>/registos/', views.ListREGISTOS.as_view()),
+    path('parcelas/<int:parcelas_id>/registos/<int:registos_id>/', views.DetailREGISTOS.as_view()),
 
 ]
